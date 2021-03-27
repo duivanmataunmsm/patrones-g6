496ae111fdb296be84dffebb216612f8ce85d232
kevip <castaneda.kevin114@gmail.com>
2021-03-27 12:13:40 -0500

    Pruebas unitarias de chessBuilder

---
+++ src/test/java/com/edu/grupo6/builder/ChessGamePieceBuilderTest.java
@@ -0,0 +1,53 @@
+package com.edu.grupo6.builder;
+
+import com.edu.grupo6.ChessGameBoard;
+import com.edu.grupo6.ChessGamePiece;
+import com.edu.grupo6.enums.PieceType;
+import com.edu.grupo6.models.King;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+
+class ChessGamePieceBuilderTest {
+
+    @Test
+    void shouldExpectSpecificPiece() {
+        ChessGameBoard board = new ChessGameBoard();
+        ChessGamePiece piece;
+        ChessGamePieceBuilder builder = new ChessGamePieceBuilder(board, PieceType.KING);
+        piece = builder.build();
+        Assertions.assertInstanceOf(King.class, piece);
+    }
+    @Test
+    void shouldExpectBlackPiece() {
+        ChessGameBoard board = new ChessGameBoard();
+        ChessGamePiece piece;
+        ChessGamePieceBuilder builder = new ChessGamePieceBuilder(board, PieceType.KING);
+        piece = builder.withColor(0).build();
+        Assertions.assertEquals(0, piece.getColorOfPiece());
+    }
+    @Test
+    void shouldExpectWhitePiece() {
+        ChessGameBoard board = new ChessGameBoard();
+        ChessGamePiece piece;
+        ChessGamePieceBuilder builder = new ChessGamePieceBuilder(board, PieceType.KING);
+        piece = builder.withColor(1).build();
+        Assertions.assertEquals(1, piece.getColorOfPiece());
+    }
+
+    @Test
+    void shouldExpectColumn() {
+        ChessGameBoard board = new ChessGameBoard();
+        ChessGamePiece piece;
+        ChessGamePieceBuilder builder = new ChessGamePieceBuilder(board, PieceType.KING);
+        piece = builder.withColumn(1).build();
+        Assertions.assertEquals(1, piece.getColumn());
+    }
+
+    @Test
+    void withRow() {
+        Assertions.assertEquals(1, 1);
+    }
+
+
+}
