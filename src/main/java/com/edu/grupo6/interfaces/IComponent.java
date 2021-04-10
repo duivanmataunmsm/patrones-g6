bfb0e7426b1bf69f180ae0ce7d474e2fd08eaf83
kevip <castaneda.kevin114@gmail.com>
2021-04-10 04:19:16 -0500

    Se agregan interfaces y clas mediator

---
+++ src/main/java/com/edu/grupo6/interfaces/IComponent.java
@@ -0,0 +1,6 @@
+package com.edu.grupo6.interfaces;
+
+public interface Component {
+
+    public void setMediator();
+}
---
+++ src/main/java/com/edu/grupo6/interfaces/IGUIMediator.java
@@ -0,0 +1,2 @@
+package com.edu.grupo6.impl;public interface GUIMediator {
+}
