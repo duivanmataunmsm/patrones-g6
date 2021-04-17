a4cbd7394cfa1fcce45cdb6add8f34d9c3f96908
ericvipo <ericvpoma@gmail.com>
2021-04-17 09:28:10 -0500

    patron object pool

---
+++ src/main/java/com/edu/grupo6/Mensaje.java
@@ -0,0 +1,23 @@
+import java.time.LocalDateTime;
+
+public class Mensaje {
+  private String content;
+  private LocalDateTime _createdAt = LocalDateTime.now();
+
+  public Mensaje(String content) {
+    this.content = content;
+  }
+
+  public LocalDateTime getCreatedAt() {
+    return _createdAt;
+  }
+
+  public void setContent(String content) {
+    this.content = content;
+  }
+
+  public String getContent() {
+    return content;
+  }
+
+}
